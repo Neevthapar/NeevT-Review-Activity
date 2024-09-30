76201116e8c5b72d44c017e5486cd755db8dc06b
Neevthapar <neevthapar02@gmail.com>
2024-09-30 11:34:03 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').innerHTML = 'The text has been changed!';
+});
+
+document.getElementById('Button1').addEventListener('click', function() {
+    document.getElementById('First').innerHTML = 'The text has been changed!';
+});
+
+document.getElementById('Button2').addEventListener('click', function() {
+    document.getElementById('Second').innerHTML = 'The text has been changed!';
+});
